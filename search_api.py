c83f9621a2c630dea19a61a3b6bbd74c3e92b796
Josh Montague <joshua.montague@gmail.com>
2013-11-12 23:48:02 +0000

    add -q query option to see request, fix rate calculation

--- search_api.py
+++ search_api.py
@@ -33,16 +33,13 @@ class GnipSearchAPI:
                 default="https://search.gnip.com/accounts/shendrickson/publishers/twitter/search/wayback.json",
                 help="Url of search endpoint. (See your Gnip console.)")
         twitter_parser.add_argument("-s", "--start-date", dest="start", 
-                #default="2013-01-01T01:01",
                 default=None,
                 help="Start of datetime window, format 'YYYY-mm-DDTHH:MM' (default: 30 days ago)")
         twitter_parser.add_argument("-e", "--end-date", dest="end", 
                 default=None,
-                help="End of datetime window, format 'YYYY-mm-DDTHH:MM' (default: none)")
-#        # query test option
-#        twitter_parser.add_argument("-q", "--query", dest="query", action="store_true", 
-#                default=False, help="View API query")
-#        #
+                help="End of datetime window, format 'YYYY-mm-DDTHH:MM' [Omit for most recent activities] (default: none)")
+        twitter_parser.add_argument("-q", "--query", dest="query", action="store_true", 
+                default=False, help="View API query (no data)")
         twitter_parser.add_argument("-u", "--user-name", dest="user", default="shendrickson@gnip.com",
                 help="User name")
         twitter_parser.add_argument("-p", "--password", dest="pwd", 
@@ -70,28 +67,27 @@ class GnipSearchAPI:
             self.index = DATE_INDEX
         elif self.options.use_case.startswith("link"):
             self.index = LINKS_INDEX
-        # time window parsing 
-        timeRE = re.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}.[0-9]{2}:[0-9]{2}")
-        input_dt_fmt = '%Y-%m-%dT%H:%M'
-        query_dt_fmt = '%Y%m%d%H%M'
+        timeRE = re.compile("([0-9]{4}).([0-9]{2}).([0-9]{2}).([0-9]{2}):([0-9]{2})")
         if self.options.start:
             dt = re.search(timeRE, self.options.start)
             if not dt:
                 print >> sys.stderr, "Error. Invalid start-date format: %s \n"%str(self.options.start)
                 sys.exit()    
             else:
-                # from '2013-11-08T15:18' to '201311081518' 
-                tmp = datetime.datetime.strptime(dt.group(0), input_dt_fmt)
-                self.fromDate = tmp.strftime(query_dt_fmt) 
+                f =''
+                for i in range(re.compile(timeRE).groups):
+                    f += dt.group(i+1) 
+                self.fromDate = f
         if self.options.end:
-            # do we make an n=10 request to find out the server time? 
             dt = re.search(timeRE, self.options.end)
             if not dt:
                 print >> sys.stderr, "Error. Invalid end-date format: %s \n"%str(self.options.end)
                 sys.exit()
             else:
-                tmp = datetime.datetime.strptime(dt.group(0), input_dt_fmt)
-                self.toDate = tmp.strftime(query_dt_fmt) 
+                e =''
+                for i in range(re.compile(timeRE).groups):
+                    e += dt.group(i+1) 
+                self.toDate = e
 
 
     def req(self):
@@ -130,13 +126,13 @@ class GnipSearchAPI:
             self.rule_payload["fromDate"] = self.fromDate
         if self.options.end:
             self.rule_payload["toDate"] = self.toDate
-#        if self.options.query:
-#            print >>sys.stdout, "Constructed API query: "
-#            print >>sys.stdout, "%s\n"%self.rule_payload
-#            sys.exit()
-#        #
+        if self.options.query:
+            print >>sys.stderr, "API query:"
+            print >>sys.stderr, self.rule_payload
+            sys.exit() 
+        #
         self.oldest_t = datetime.datetime.utcnow()
-        start_t = self.oldest_t
+        self.newest_t = datetime.datetime(year=1900,month=01,day=01)
         self.doc = []
         self.res_cnt = 0
         for rec in self.parse_JSON(self.req()):
@@ -146,7 +142,9 @@ class GnipSearchAPI:
                 t = datetime.datetime.strptime(t_str,"%Y-%m-%dT%H:%M:%S.000Z")
                 if t < self.oldest_t:
                     self.oldest_t = t
-                    self.delta_t = (start_t - self.oldest_t).total_seconds()/60.
+                elif t > self.newest_t:
+                    self.newest_t = t
+                self.delta_t = (self.newest_t - self.oldest_t).total_seconds()/60.
             elif self.options.use_case.startswith("links"):
                 link_str = self.twitter_parser.procRecordToList(rec)[self.index]
                 if link_str != "None":
@@ -172,14 +170,12 @@ class GnipSearchAPI:
                 unit = "Tweets/Hour"
             res.append("   PowerTrack Rule: \"%s\""%self.options.filter)
             res.append("Oldest Tweet (UTC): %s"%str(self.oldest_t))
+            res.append("Newest Tweet (UTC): %s"%str(self.newest_t))
             res.append("         Now (UTC): %s"%str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")))
             res.append("      %5d Tweets: %6.3f %s"%(self.res_cnt, rate, unit))
             res.append("-"*WIDTH)
         elif self.options.use_case.startswith("json"):
             res.extend(self.doc)
-            #
-            #sys.stderr.write('self.doc: %s \n'%str(self.doc))
-            #
         elif self.options.use_case.startswith("word") or self.options.use_case.startswith("user"):
             res.append("%22s -- %10s     %8s (%d)"%( "terms", "mentions", "activities", self.res_cnt))
             res.append("-"*WIDTH)
