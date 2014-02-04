f72bd958269ae2068fc26ef48fd5116f5787fc4a
Josh Montague <joshua.montague@gmail.com>
2014-02-04 14:48:05 -0700

    increase readability of rule_payload

--- search_api.py
+++ search_api.py
@@ -136,7 +136,11 @@ class GnipSearchAPI:
         return acs
 
     def __call__(self):
-        self.rule_payload = {'query':self.options.filter, 'maxResults': int(self.options.max), 'publisher': 'twitter'}
+        self.rule_payload = {
+                                'query':self.options.filter
+                                , 'maxResults': int(self.options.max)
+                                , 'publisher': 'twitter'
+                            }
         if self.options.start:
             self.rule_payload["fromDate"] = self.fromDate
         if self.options.end:
