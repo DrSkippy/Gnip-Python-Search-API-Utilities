0f8974564d534022deff1893b3a94c55862ebdab
Scott Hendrickson <scott@drskippy.net>
2014-11-11 14:14:30 -0700

    Library class and separation of concerns

--- search_api.py
+++ search_api.py
@@ -3,7 +3,6 @@
 __author__="Scott Hendrickson, Josh Montague" 
 
 import sys
-import requests
 import json
 import codecs
 import argparse
@@ -11,40 +10,24 @@ import datetime
 import time
 import os
 import ConfigParser
-import re
 
-try:
-    from acscsv.twitter_acs import TwacsCSV
-except ImportError:
-    # previous versions of gnacs used a different module name
-    from acscsv.twacscsv import TwacsCSV
-from simple_n_grams.simple_n_grams import SimpleNGrams
+from gnip_search.gnip_search_api import *
 
 reload(sys)
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 sys.stdin = codecs.getreader('utf-8')(sys.stdin)
 
-# formatter of data from API 
-TIME_FMT = "%Y%m%d%H%M"
-PAUSE = 3 # seconds between page requests
 DEFAULT_CONFIG_FILENAME = "./.gnip"
 
-class GnipSearchAPI:
+class GnipSearchCMD(GnipSearchAPI):
 
     USE_CASES = ["json", "wordcount","users", "rate", "links", "timeline", "geo"]
     
     def __init__(self, token_list_size=20):
-        # Some constants to configure column retrieval
-        DATE_INDEX = 1
-        TEXT_INDEX = 2
-        LINKS_INDEX = 3
-        USER_NAME_INDEX = 7 
         # default tokenizer and character limit
         space_tokenizer = False
         char_upper_cutoff = 20  # longer than for normal words because of user names
         self.token_list_size = int(token_list_size)
-        # re for the acceptable datetime formats
-        timeRE = re.compile("([0-9]{4}).([0-9]{2}).([0-9]{2}).([0-9]{2}):([0-9]{2})")
         #############################################
         # CONFIG FILE/COMMAND LINE OPTIONS PATTERN
         # parse config file
@@ -75,50 +58,15 @@ class GnipSearchAPI:
             self.password = self.options.password
         if self.options.stream_url is not None:
             self.stream_url = self.options.stream_url
-        #
         #############################################
-        if self.options.use_case.startswith("links"):
-            char_upper_cutoff=100
-            space_tokenizer = True
-        self.freq = SimpleNGrams(charUpperCutoff=char_upper_cutoff, space_tokenizer=space_tokenizer)
-        if self.options.use_case.startswith("user"):
-            self.index = USER_NAME_INDEX
-        elif self.options.use_case.startswith("wordc"):
-            self.index = TEXT_INDEX
-        elif self.options.use_case.startswith("rate"):
-            self.index = DATE_INDEX
-        elif self.options.use_case.startswith("link"):
-            self.index = LINKS_INDEX
-        elif self.options.use_case.startswith("time"):
-            if not self.stream_url.endswith("counts.json"): 
-                self.stream_url = self.stream_url[:-5] + "/counts.json"
-            if self.options.count_bucket not in ['day', 'minute', 'hour']:
-                print >> sys.stderr, "Error. Invalid count bucket: %s \n"%str(self.options.count_bucket)
-                sys.exit()
-        if self.options.start:
-            dt = re.search(timeRE, self.options.start)
-            if not dt:
-                print >> sys.stderr, "Error. Invalid start-date format: %s \n"%str(self.options.start)
-                sys.exit()    
-            else:
-                f =''
-                for i in range(re.compile(timeRE).groups):
-                    f += dt.group(i+1) 
-                self.fromDate = f
-        if self.options.end:
-            dt = re.search(timeRE, self.options.end)
-            if not dt:
-                print >> sys.stderr, "Error. Invalid end-date format: %s \n"%str(self.options.end)
-                sys.exit()
-            else:
-                e =''
-                for i in range(re.compile(timeRE).groups):
-                    e += dt.group(i+1) 
-                self.toDate = e
-        self.name_munger(self.options.filter)
-        # get a parser for the twitter columns
-        # TODO: use the updated retriveal methods in gnacs instead of this
-        self.twitter_parser = TwacsCSV(",", None, False, True, False, True, False, False, False)
+        super(GnipSearchCMD, self).__init__(
+            self.user
+            , self.password
+            , self.stream_url
+            , self.options.paged
+            , self.options.output_file_path
+            , self.token_list_size
+            )
 
     def config_file(self):
         config = ConfigParser.ConfigParser()
@@ -170,190 +118,16 @@ class GnipSearchAPI:
                 help="Create files in ./OUTPUT-FILE-PATH. This path must exists and will not be created. This options is available only with -a option. Default is no output files.")
         return twitter_parser
     
-    def name_munger(self, f):
-        """Creates a file name per input rule when reading multiple input rules."""
-        f = re.sub(' +','_',f)
-        f = f.replace(':','_')
-        f = f.replace('"','_Q_')
-        f = f.replace('(','_p_') 
-        f = f.replace(')','_p_') 
-        self.file_name_prefix = f[:42]
-
-    def req(self):
-        try:
-            s = requests.Session()
-            s.headers = {'Accept-encoding': 'gzip'}
-            s.auth = (self.user, self.password)
-            res = s.post(self.stream_url, data=json.dumps(self.rule_payload))
-        except requests.exceptions.ConnectionError, e:
-            print >> sys.stderr, "Error (%s). Exiting without results."%str(e)
-            sys.exit()
-        except requests.exceptions.HTTPError, e:
-            print >> sys.stderr, "Error (%s). Exiting without results."%str(e)
-            sys.exit()
-        return res.text
-
-    def parse_JSON(self):
-        acs = []
-        repeat = True
-        page_count = 0
-        while repeat:
-            doc = self.req()
-            try:
-                tmp_response =  json.loads(doc)
-                if "results" in tmp_response:
-                    acs.extend(tmp_response["results"])
-                if "error" in tmp_response:
-                    print >> sys.stderr, "Error, invalid request"
-                    print >> sys.stderr, "Query: %s"%self.rule_payload
-                    print >> sys.stderr, "Response: %s"%doc
-            except ValueError:
-                print >> sys.stderr, "Error, results not parsable"
-                print >> sys.stderr, doc
-                sys.exit()
-            # 
-            repeat = False
-            if self.options.paged:
-                if len(acs) > 0:
-                    if self.options.output_file_path is not None:
-                        file_name = self.options.output_file_path + "/{0}_{1}.json".format(
-                                str(datetime.datetime.utcnow().strftime(
-                                    "%Y%m%d%H%M%S"))
-                              , str(self.file_name_prefix))
-                        with codecs.open(file_name, "wb","utf-8") as out:
-                            print >> sys.stderr, "(writing to file ...)"
-                            for item in tmp_response["results"]:
-                                out.write(json.dumps(item)+"\n")
-                    else:
-                        # if writing to file, don't keep track of all the data in memory
-                        acs = []
-                else:
-                    print >> sys.stderr, "no results returned for rule:{0}".format(str(self.rule_payload))
-                if "next" in tmp_response:
-                    self.rule_payload["next"]=tmp_response["next"]
-                    repeat = True
-                    page_count += 1
-                    print >> sys.stderr, "Fetching page {}...".format(page_count)
-                else:
-                    if "next" in self.rule_payload:
-                        del self.rule_payload["next"]
-                    repeat = False
-                time.sleep(PAUSE)
-        return acs
-
     def __call__(self):
-        if self.options.paged:
-            # avoid making many small requests
-            self.options.max = 500
-        self.rule_payload = {
-                                'query': self.options.filter
-                         , 'maxResults': int(self.options.max)
-                          , 'publisher': 'twitter'
-                            }
-        if self.options.start:
-            self.rule_payload["fromDate"] = self.fromDate
-        if self.options.end:
-            self.rule_payload["toDate"] = self.toDate
-        if self.options.use_case.startswith("time"):
-            self.rule_payload["bucket"] = self.options.count_bucket
-        if self.options.query:
-            print >>sys.stderr, "API query:"
-            print >>sys.stderr, self.rule_payload
-            sys.exit() 
-        #
-        self.doc = []
-        self.res_cnt = 0
-        self.delta_t = 1    # keeps non-'rate' use-cases from crashing 
-        # default delta_t = 30d & search only goes back 30 days
-        self.oldest_t = datetime.datetime.utcnow() - datetime.timedelta(days=30)
-        self.newest_t = datetime.datetime.utcnow()
-        for rec in self.parse_JSON():
-            self.res_cnt += 1
-            if self.options.use_case.startswith("rate"):
-                t_str = self.twitter_parser.procRecordToList(rec)[self.index]
-                t = datetime.datetime.strptime(t_str,"%Y-%m-%dT%H:%M:%S.000Z")
-                if t < self.oldest_t:
-                    self.oldest_t = t
-                if t > self.newest_t:
-                    self.newest_t = t
-                self.delta_t = (self.newest_t - self.oldest_t).total_seconds()/60.
-            elif self.options.use_case.startswith("links"):
-                link_str = self.twitter_parser.procRecordToList(rec)[self.index]
-                if link_str != "None":
-                    exec("link_list=%s"%link_str)
-                    for l in link_list:
-                        self.freq.add(l)
-                else:
-                    self.freq.add("NoLinks")
-            elif self.options.use_case.startswith("json"):
-                self.doc.append(json.dumps(rec))
-            elif self.options.use_case.startswith("geo"):
-                lat, lng = None, None
-                if "geo" in rec:
-                    if "coordinates" in rec["geo"]:
-                        [lat,lng] = rec["geo"]["coordinates"]
-                record = { "id": rec["id"].split(":")[2]
-                        , "postedTime": rec["postedTime"].strip(".000Z")
-                        , "latitude": lat
-                        , "longitude": lng }
-                self.doc.append(record)
-                #self.doc.append(json.dumps(record))
-            elif self.options.use_case.startswith("time"):
-                self.doc.append(rec)
-            else:
-                self.freq.add(self.twitter_parser.procRecordToList(rec)[self.index])
-        return self
-
-    def get_repr(self):
-        WIDTH = 60
-        res = [u"-"*WIDTH]
-        if self.options.use_case.startswith("rate"):
-            rate = float(self.res_cnt)/self.delta_t
-            unit = "Tweets/Minute"
-            if rate < 0.01:
-                rate *= 60.
-                unit = "Tweets/Hour"
-            res.append("   PowerTrack Rule: \"%s\""%self.options.filter)
-            res.append("Oldest Tweet (UTC): %s"%str(self.oldest_t))
-            res.append("Newest Tweet (UTC): %s"%str(self.newest_t))
-            res.append("         Now (UTC): %s"%str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")))
-            res.append("      %5d Tweets: %6.3f %s"%(self.res_cnt, rate, unit))
-            res.append("-"*WIDTH)
-        elif self.options.use_case.startswith("geo"):
-            if self.options.csv_flag:
-                res = []
-                for x in self.doc:
-                    try:
-                        res.append("{},{},{},{}".format(x["id"], x["postedTime"], x["longitude"], x["latitude"]))
-                    except KeyError, e:
-                        print >> sys.stderr, str(e)
-            else:
-                res = [json.dumps(x) for x in self.doc]
-        elif self.options.use_case.startswith("json"):
-            res = self.doc
-        elif self.options.use_case.startswith("word") or self.options.use_case.startswith("user"):
-            res.append("%22s -- %10s     %8s (%d)"%( "terms", "mentions", "activities", self.res_cnt))
-            res.append("-"*WIDTH)
-            for x in self.freq.get_tokens(self.token_list_size):
-                res.append("%22s -- %4d  %5.2f%% %4d  %5.2f%%"%(x[4], x[0], x[1]*100., x[2], x[3]*100.))
-            res.append("-"*WIDTH)
-        elif self.options.use_case.startswith("time"):
-            if self.options.csv_flag:
-                res = ["{:%Y-%m-%dT%H:%M:%S},{}".format(
-                    datetime.datetime.strptime(x["timePeriod"]
-                  , TIME_FMT)
-                  , x["count"]) 
-                        for x in self.doc]
-            else:
-                res = [json.dumps({"results": self.doc})] 
-        else:
-            res[-1]+=u"-"*WIDTH
-            res.append("%100s -- %10s     %8s (%d)"%("links", "mentions", "activities", self.res_cnt))
-            res.append("-"*2*WIDTH)
-            for x in self.freq.get_tokens(self.token_list_size):
-                res.append("%100s -- %4d  %5.2f%% %4d  %5.2f%%"%(x[4], x[0], x[1]*100., x[2], x[3]*100.))
-            res.append("-"*WIDTH)
-        return "\n".join(res)
-
+        return self.query_api(
+            self.options.filter
+            , self.options.max
+            , self.options.use_case
+            , self.options.start
+            , self.options.end
+            , self.options.count_bucket 
+            , self.options.csv_flag
+            , self.options.query)
+    
 if __name__ == "__main__":
-    print GnipSearchAPI()().get_repr()
+    print GnipSearchCMD()()
--- timeline_plots/plot.sh
+++ timeline_plots/plot.sh
@@ -5,11 +5,12 @@
 #
 # Be sure the export GNIP_CONFIG or have your .gnip in the local folder
 # Set this or export env variable
-SEARCH_PATH=/Users/scotthendrickson/IdeaProjects/Gnip-Python-Search-API-Utilities
-SEARCH_PATH=/home/scott/Projects/Gnip-Python-Search-API-Utilities
+GNIP_SEARCH_PATH=/Users/shendrickson/workspace/Gnip-Python-Search-API-Utilities
+#GNIP_SEARCH_PATH=/home/scott/workspace/Gnip-Python-Search-API-Utilities
 # can use minute, hour, day for bucket size
 BUCKET_SIZE=hour
-$SEARCH_PATH/search_api.py -f"$1" -cb$BUCKET_SIZE timeline > data.csv
+$GNIP_SEARCH_PATH/search_api.py -f"$1" -cb$BUCKET_SIZE timeline > data.csv
 ./plot.r data.csv "$1" "$1" "$BUCKET_SIZE"
+cat data.csv | cut -d, -f2 | /Users/shendrickson/workspace/CollectorUtils/stats.py
 # on OSX, if you want to immediatly see your plot
 open "$1".png
