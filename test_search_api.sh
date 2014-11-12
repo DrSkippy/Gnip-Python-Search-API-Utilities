f67894d19d5893d75983a220b26e3e3ae4131fc7
Scott Hendrickson <scott@drskippy.net>
2014-11-11 18:38:47 -0700

    All test running (more reasonable data pull)

--- test_search_api.sh
+++ test_search_api.sh
@@ -12,34 +12,39 @@ if [ ! -d data ]; then
     mkdir data
 fi
 
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 -q json
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 json
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 geo
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 wordcount
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 timeline
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 users
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 -c geo
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 -c timeline 
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 -s"2014-05-15T12:30" -e"2014-05-16T12:30" json
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 -s"2014-05-15T12:30" -e"2014-05-16T12:30" geo
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 -s"2014-05-15T12:30" -e"2014-05-16T12:30" wordcount
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -n10 -s"2014-05-15T12:30" -e"2014-05-16T12:30" users
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -s"2014-05-25T12:30" -e"2014-06-10T12:30" -aw json
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -s"2014-05-25T12:30" -e"2014-06-10T12:30" -aw geo
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)" -u ${un} -p ${paswd} -s"2014-05-25T12:30" -e"2014-06-10T12:30" -a users
+rulez="bieber OR bieber"
+dt1=$(date -v-1d +%Y-%m-%dT00:00:00)
+dt2=$(date -v-2d +%Y-%m-%dT00:00:00)
+dt3=$(date -v-2d +%Y-%m-%dT23:55:00)
+
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 -q json
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 json
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 geo
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 wordcount
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 timeline
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 users
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 -c geo
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 -c timeline 
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 -s"$dt2" -e"$dt1" json
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 -s"$dt2" -e"$dt1" geo
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 -s"$dt2" -e"$dt1" wordcount
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -n10 -s"$dt2" -e"$dt1" users
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -s"$dt3" -e"$dt1" -aw ./data json
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -s"$dt3" -e"$dt1" -aw ./data geo
+./search_api.py -f"has:geo $rulez" -u ${un} -p ${paswd} -s"$dt3" -e"$dt1" -a users
 
 export GNIP_CONFIG_FILE=./.gnip
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 -q json
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 json
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 geo
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 wordcount
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 timeline
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 users
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 -c geo
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 -c timeline 
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 -s"2014-05-15T12:30" -e"2014-05-16T12:30" json
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 -s"2014-05-15T12:30" -e"2014-05-16T12:30" geo
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 -s"2014-05-15T12:30" -e"2014-05-16T12:30" wordcount
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -n10 -s"2014-05-15T12:30" -e"2014-05-16T12:30" users
-./search_api.py -f"has:geo (stanleycup OR stanley cup) (kings OR rangers)"  -s"2014-05-25T12:30" -e"2014-06-10T12:30" -aw json
+./search_api.py -f"has:geo $rulez"  -n10 -q json
+./search_api.py -f"has:geo $rulez"  -n10 json
+./search_api.py -f"has:geo $rulez"  -n10 geo
+./search_api.py -f"has:geo $rulez"  -n10 wordcount
+./search_api.py -f"has:geo $rulez"  -n10 timeline
+./search_api.py -f"has:geo $rulez"  -n10 users
+./search_api.py -f"has:geo $rulez"  -n10 -c geo
+./search_api.py -f"has:geo $rulez"  -n10 -c timeline 
+./search_api.py -f"has:geo $rulez"  -n10 -s"$dt2" -e"$dt1" json
+./search_api.py -f"has:geo $rulez"  -n10 -s"$dt2" -e"$dt1" geo
+./search_api.py -f"has:geo $rulez"  -n10 -s"$dt2" -e"$dt1" wordcount
+./search_api.py -f"has:geo $rulez"  -n10 -s"$dt2" -e"$dt1" users
+./search_api.py -f"has:geo $rulez"  -s"$dt3" -e"$dt1" -aw ./data json
 
